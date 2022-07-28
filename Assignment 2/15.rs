d080e9cacbbbe97422d3a3349a8ceb35c92dc60e
Ryan Chowdhury <44534254+ryan-preneur@users.noreply.github.com>
2022-07-28 22:42:56 +0530

    Add files via upload

---
+++ Assignment 2/15.rs
@@ -0,0 +1,7 @@
+/* Make it work, only using comments! */
+fn main() {
+    todo!();
+    // unimplemented!();
+
+    // assert_eq!(6, 5 + 3 + 2 + 1 )
+}
---
+++ Assignment 2/16.1.rs
@@ -0,0 +1,20 @@
+// 1.
+
+
+
+fn main() {
+    let s1 = "hello";
+    /* Fill in the blank */
+    let s = format!("{}, world!", s1);
+    assert_eq!(s, "hello, world!");
+}
+
+
+// 2.
+
+
+fn main() {
+    print!("hello world, ");
+    println!("I am");
+    println!("Sunface!");
+}
